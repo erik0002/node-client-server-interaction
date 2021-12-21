0402d548fd7ae0932596b66ad7be237deec8b579
Yeliazar <erik.ruzhakov@mail.ru>
2021-12-22 00:02:02 +0300

    adding project

---
+++ routes/todo.js
@@ -0,0 +1,61 @@
+const {Router} = require('express')
+const Todo = require('../models/todo')
+const router = Router()
+
+// получение списка задач
+router.get('/', async (req, res) => {
+    try {
+       const todos =  await Todo.findAll()
+       res.status(200).json(todos)
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+//Создание новой задачи
+router.post('/', async(req, res) => {
+    try {
+       const todo = await  Todo.create({
+            title: req.body.title,
+            done: false
+        })
+
+        res.status(201).json({todo})
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+//Изменение задачи
+router.put('/:id', (req, res) => {
+    try {
+
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+// Удаление задачи
+router.delete('/:id', (req, res) => {
+    try {
+
+    } catch(e) {
+        console.log(e)
+        res.status(500).json({
+            message: 'Server error'
+        })
+    }
+})
+
+
+
+module.exports = router
