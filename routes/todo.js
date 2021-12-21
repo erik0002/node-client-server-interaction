800e381150dae67a8175acef6afa80ed0b2d8160
Yeliazar <erik.ruzhakov@mail.ru>
2021-12-22 00:23:07 +0300

    updated router put

--- routes/todo.js
+++ routes/todo.js
@@ -33,9 +33,12 @@ router.post('/', async(req, res) => {
 })
 
 //Изменение задачи
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     try {
-
+        const todo = await Todo.findByPk(+req.params.id)
+        todo.done = req.body.done
+        await todo.save()
+        res.status(200).json({todo})
     } catch(e) {
         console.log(e)
         res.status(500).json({
